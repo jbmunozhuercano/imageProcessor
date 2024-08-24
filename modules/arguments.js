d7c697bfb2f8dbe796057d1323a719c27c1e83fa
Jorge Muñoz Huércano <jbmunoz@protonmail.com>
2024-08-24 19:29:45 +0200

    Added options and quality values

--- modules/arguments.js
+++ modules/arguments.js
@@ -1,15 +1,43 @@
+const { number } = require('yargs');
 const yargs = require('yargs/yargs');
 
-const args = yargs(process.argv.slice(2)).option('option', {
-  alias: 'o',
-  describe: 'Elige una opción',
-  choices: [
-    'reducir calidad JPG',
-    'reducir calidad PNG',
-    'cambiar extensión a JPG',
-    'convertir a webP',
-    'reducir a tamaños para Fumisan',
-  ],
-}).argv;
+const choices = [
+  'reducir calidad JPG',
+  'reducir calidad PNG',
+  'cambiar extensión a JPG',
+  'convertir a webP',
+  'reducir a tamaños para Fumisan',
+];
+
+function displayChoices() {
+  console.log('Selecciona una opción, por su índice:');
+  choices.forEach((choice, index) => {
+    console.log(`${index}: ${choice}`);
+  });
+}
+
+displayChoices();
+
+const args = yargs(process.argv.slice(2))
+  .option('option', {
+    alias: 'o',
+    type: 'number',
+    describe: 'Selecciona una opción',
+    choices: Array.from(
+      {
+        length: choices.length,
+      },
+      (_, i) => i
+    ),
+    demandOption: true,
+  })
+  .option('quality', {
+    alias: 'q',
+    type: 'number',
+    describe: 'Selecciona calidad 0-100',
+    demandOption: false,
+  })
+  .help()
+  .alias('help', 'h').argv;
 
 module.exports = args;
